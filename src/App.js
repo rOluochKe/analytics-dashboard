9cf8a05fea44cef0364ede236856e7a8a910ac42
Raymond Oluoch <rasholuoch@gmail.com>
2021-06-24 19:32:07 +0300

    navigation links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Topbar from './components/topbar/Topbar'
+import Sidebar from './components/sidebar/Sidebar'
+
+import './app.css'
 
 const App = () => {
   return (
     <div>
       <Topbar />
+      <div className='container'>
+        <Sidebar />
+      </div>
     </div>
   )
 }
